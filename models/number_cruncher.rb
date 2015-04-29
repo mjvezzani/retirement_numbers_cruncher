0a7444e047cd425da8b6c7f46be8b283c51bd9b1
Michael Vezzani <mjvezzani@gmail.com>
2015-04-29 15:49:12 -0400

    Write tests for income source type pension and get them passing.

--- models/number_cruncher.rb
+++ models/number_cruncher.rb
@@ -7,7 +7,7 @@ class NumberCruncher
     @retirement_income = args[:retirement_income]
   end
 
-  def crunch_ss_numbers
+  def crunch_numbers
     time_passed = retirement_income.start_age - user.current_age
     amount_dispersed = retirement_income.annual_amount
     time_passed.times do
--- spec/number_cruncher_spec.rb
+++ spec/number_cruncher_spec.rb
@@ -5,26 +5,40 @@ require 'rspec'
 
 describe NumberCruncher do
 
-  it 'determines how much a user will collect in social security when they retire' do
-    user = User.new(current_age: 30, retirement_age: 60)
-    retirement_income = RetirementIncome.new(type: 'ss1',
+  before :each do
+    @user = User.new(current_age: 30, retirement_age: 60)
+    @retirement_income = RetirementIncome.new(type: 'ss1',
                                              start_age: 60,
                                              annual_amount: 24000)
-    number_cruncher = NumberCruncher.new(user: user,
-                                         retirement_income: retirement_income)
+    @number_cruncher = NumberCruncher.new(user: @user,
+                                         retirement_income: @retirement_income)
+  end
 
-    expect(number_cruncher.crunch_ss_numbers).to eq 50341.62
+  it 'determines how much a user will collect in social security 1 when they retire' do
+    expect(@number_cruncher.crunch_numbers).to eq 50341.62
   end
 
   it 'determines how much a user will collect in social security 2 when they retire' do
-    user = User.new(current_age: 30, retirement_age: 60)
-    retirement_income = RetirementIncome.new(type: 'ss2',
-                                             start_age: 65,
-                                             annual_amount: 10000)
-    number_cruncher = NumberCruncher.new(user: user,
-                                         retirement_income: retirement_income)
+    @retirement_income.start_age = 65
+    @retirement_income.annual_amount = 10000
 
-    expect(number_cruncher.crunch_ss_numbers).to eq 23732.05
+    expect(@number_cruncher.crunch_numbers).to eq 23732.05
+  end
+
+  it 'determines how much a user will collect in pension 1 when they retire' do
+    @retirement_income.type = 'pension1'
+    @retirement_income.start_age = 60
+    @retirement_income.annual_amount = 15000
+
+    expect(@number_cruncher.crunch_numbers).to eq 31463.51
+  end
+
+  it 'determines how much a user will collect in pension 2 when they retire' do
+    @retirement_income.type = 'pension2'
+    @retirement_income.start_age = 63
+    @retirement_income.annual_amount = 3000
+
+    expect(@number_cruncher.crunch_numbers).to eq 6776.55
   end
 
 end
