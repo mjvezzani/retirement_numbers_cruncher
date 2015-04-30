1b0dd7ed90dfb254498aea6d433fa40a2c3712bd
Michael Vezzani <mjvezzani@gmail.com>
2015-04-30 10:35:53 -0400

    Write test to sum all retirement income types together for a given age. Write code to get them passing.

--- models/number_cruncher.rb
+++ models/number_cruncher.rb
@@ -7,13 +7,85 @@ class NumberCruncher
     @retirement_income = args[:retirement_income]
   end
 
-  def crunch_numbers
-    time_passed = retirement_income.start_age - user.current_age
-    amount_dispersed = retirement_income.annual_amount
+  def crunch_ss1_numbers
+    income_to_crunch = retirement_income.select{|x| x.type == 'SS1'}
+    time_passed = income_to_crunch[0].start_age - user.current_age
+    amount_dispersed = income_to_crunch[0].annual_amount
     time_passed.times do
       amount_dispersed *= 1.025
     end
     amount_dispersed.round(2)
+  end
+
+  def crunch_ss2_numbers
+    income_to_crunch = retirement_income.select{|x| x.type == 'SS2'}
+    time_passed = income_to_crunch[0].start_age - user.current_age
+    amount_dispersed = income_to_crunch[0].annual_amount
+    time_passed.times do
+      amount_dispersed *= 1.025
+    end
+    amount_dispersed.round(2)
+  end
+
+  def crunch_pension1_numbers
+    income_to_crunch = retirement_income.select{|x| x.type == 'PENSION1'}
+    time_passed = income_to_crunch[0].start_age - user.current_age
+    amount_dispersed = income_to_crunch[0].annual_amount
+    time_passed.times do
+      amount_dispersed *= 1.025
+    end
+    amount_dispersed.round(2)
+  end
+
+  def crunch_pension2_numbers
+    income_to_crunch = retirement_income.select{|x| x.type == 'PENSION2'}
+    time_passed = income_to_crunch[0].start_age - user.current_age
+    amount_dispersed = income_to_crunch[0].annual_amount
+    time_passed.times do
+      amount_dispersed *= 1.025
+    end
+    amount_dispersed.round(2)
+  end
+
+  def crunch_pension2_numbers
+    income_to_crunch = retirement_income.select{|x| x.type == 'PENSION2'}
+    time_passed = income_to_crunch[0].start_age - user.current_age
+    amount_dispersed = income_to_crunch[0].annual_amount
+    time_passed.times do
+      amount_dispersed *= 1.025
+    end
+    amount_dispersed.round(2)
+  end
+
+  def crunch_ptw1_numbers
+    income_to_crunch = retirement_income.select{|x| x.type == 'PTWORK1'}
+    time_passed = income_to_crunch[0].start_age - user.current_age
+    amount_dispersed = income_to_crunch[0].annual_amount
+    time_passed.times do
+      amount_dispersed *= 1.025
+    end
+    amount_dispersed.round(2)
+  end
+
+  def crunch_ptw2_numbers
+    income_to_crunch = retirement_income.select{|x| x.type == 'PTWORK2'}
+    time_passed = income_to_crunch[0].start_age - user.current_age
+    amount_dispersed = income_to_crunch[0].annual_amount
+    time_passed.times do
+      amount_dispersed *= 1.025
+    end
+    amount_dispersed.round(2)
+  end
+
+  def total_income_sources(age)
+    time_passed = age - user.current_age
+    total_amount = 0
+    qualified_income = retirement_income.select{|x| x.start_age <= age}
+    qualified_income.each{|x| total_amount += x.annual_amount}
+    time_passed.times do
+      total_amount *= 1.025
+    end
+    total_amount.round(2)
   end
 
 end
--- spec/number_cruncher_spec.rb
+++ spec/number_cruncher_spec.rb
@@ -6,55 +6,56 @@ require 'rspec'
 describe NumberCruncher do
 
   before :each do
+    ss1_income = RetirementIncome.new(type: 'SS1',
+                                      start_age: 60,
+                                      annual_amount: 24000)
+    ss2_income = RetirementIncome.new(type: 'SS2',
+                                      start_age: 65,
+                                      annual_amount: 10000)
+    pension1_income = RetirementIncome.new(type: 'PENSION1',
+                                           start_age: 60,
+                                           annual_amount: 15000)
+    pension2_income = RetirementIncome.new(type: 'PENSION2',
+                                           start_age: 63,
+                                           annual_amount: 3000)
+    ptw1_income = RetirementIncome.new(type: 'PTWORK1',
+                                       start_age: 59,
+                                       annual_amount: 14000)
+    ptw2_income = RetirementIncome.new(type: 'PTWORK2',
+                                       start_age: 61,
+                                       annual_amount: 9000)
     @user = User.new(current_age: 30, retirement_age: 60)
-    @retirement_income = RetirementIncome.new(type: 'ss1',
-                                             start_age: 60,
-                                             annual_amount: 24000)
+    @retirement_income = [ss1_income, ss2_income, pension1_income, pension2_income,
+                          ptw1_income, ptw2_income]
     @number_cruncher = NumberCruncher.new(user: @user,
                                          retirement_income: @retirement_income)
   end
 
   it 'determines how much a user will collect in social security 1 when they retire' do
-    expect(@number_cruncher.crunch_numbers).to eq 50341.62
+    expect(@number_cruncher.crunch_ss1_numbers).to eq 50341.62
   end
 
   it 'determines how much a user will collect in social security 2 when they retire' do
-    @retirement_income.start_age = 65
-    @retirement_income.annual_amount = 10000
-
-    expect(@number_cruncher.crunch_numbers).to eq 23732.05
+    expect(@number_cruncher.crunch_ss2_numbers).to eq 23732.05
   end
 
   it 'determines how much a user will collect in pension 1 when they retire' do
-    @retirement_income.type = 'pension1'
-    @retirement_income.start_age = 60
-    @retirement_income.annual_amount = 15000
-
-    expect(@number_cruncher.crunch_numbers).to eq 31463.51
+    expect(@number_cruncher.crunch_pension1_numbers).to eq 31463.51
   end
 
   it 'determines how much a user will collect in pension 2 when they retire' do
-    @retirement_income.type = 'pension2'
-    @retirement_income.start_age = 63
-    @retirement_income.annual_amount = 3000
-
-    expect(@number_cruncher.crunch_numbers).to eq 6776.55
+    expect(@number_cruncher.crunch_pension2_numbers).to eq 6776.55
   end
 
   it 'determines how much a user will collect in PT Work 1 when they retire' do
-    @retirement_income.type = 'ptw1'
-    @retirement_income.start_age = 59
-    @retirement_income.annual_amount = 14000
-
-    expect(@number_cruncher.crunch_numbers).to eq 28649.70
+    expect(@number_cruncher.crunch_ptw1_numbers).to eq 28649.70
   end
 
   it 'determines how much a user will collect in PT Work 1 when they retire' do
-    @retirement_income.type = 'ptw2'
-    @retirement_income.start_age = 61
-    @retirement_income.annual_amount = 9000 
-
-    expect(@number_cruncher.crunch_numbers).to eq 19350.06
+    expect(@number_cruncher.crunch_ptw2_numbers).to eq 19350.06
   end
 
+  it 'sums together all of the income sources for a given age' do
+    expect(@number_cruncher.total_income_sources(60)).to eq 111171.08
+  end
 end
